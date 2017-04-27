c94c5b7dcd19009a4a986940d17d5c02aec76915
bjarki15 <bjarki15@ru.is>
2017-04-27 17:55:50 +0000

    database kominn

---
+++ VerklegtNamskeid/VerklegtNamskeid/Models/UserAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VerklegtNamskeid.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int _userID { get; set; }
+        [Required(ErrorMessage = "Please insert a username")]
+        public string _userName { get; set; }
+        [Required(ErrorMessage = "Please insert an email")]
+        [DataType(DataType.EmailAddress)]
+        public string _userEmail { get; set; }
+        [Required(ErrorMessage = "Please insert a password")]
+        [DataType(DataType.Password)]
+        public string _userPassword { get; set; }
+        [Compare("_userPassword", ErrorMessage ="Please confirm password")]
+        [DataType(DataType.Password)]
+        public string _userconfirmPassword { get; set; }
+    }
+}
---
+++ VerklegtNamskeid/VerklegtNamskeid/Models/ourDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace VerklegtNamskeid.Models
+{
+    public class ourDbContext : DbContext
+    {
+        public DbSet<UserAccount> userAccount { get; set; }
+    }
+}
