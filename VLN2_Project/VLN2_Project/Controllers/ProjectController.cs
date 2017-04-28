0dd40b921e09729ce20a15163853b13fef2f5725
clover89 <clover1810@gmail.com>
2017-04-28 13:32:57 +0000

    Added service connections to controllers

--- VLN2_Project/VLN2_Project/Controllers/ProjectController.cs
+++ VLN2_Project/VLN2_Project/Controllers/ProjectController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VLN2_Project.Services;
 
 namespace VLN2_Project.Controllers
 {
     public class ProjectController : Controller
     {
+        private ProjectService _service;
+
         // GET: Project
         public ActionResult Index()
         {
